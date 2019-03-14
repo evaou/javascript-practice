cce2c0a2727a7726133348a57687c5f3e3051d4b
evaou <speeder528@gmail.com>
2019-03-14 13:43:02 +0800

    Add steps exercise

---
+++ steps/index.js
@@ -0,0 +1,67 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  const add = stair.length <= row ? '#': ' ';
+
+  return steps(n, row, stair + add);
+}
+
+module.exports = steps;
+
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let str = ''
+//
+//     for (let j = 0; j < i; j++) {
+//       str += "#";
+//     }
+//
+//     for (let j = 0; j < n - i; j++) {
+//       str += " ";
+//     }
+//
+//     console.log(str);
+//   }
+// }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//
+//     console.log(stair);
+//   }
+// }
